e56aeccc64fdcab6ba85c9a11ca353d900a46a80
Sivakumar0312 <78156874+Sivakumar0312@users.noreply.github.com>
2021-12-26 22:45:36 +0530

    Add files via upload

---
+++ ArrayLoop/ArrayLoop.java
@@ -0,0 +1,12 @@
+package Lesson12;
+
+public class ArrayLoop {
+
+	public static void main(String[] args) {
+		String[] StudenteName = {"Siva","Rahul","Raji","Shashank","Ravi","Yashoo","Vicky"};
+		for(String Name: StudenteName)
+		{
+			System.out.println(Name);}
+	}
+
+}
